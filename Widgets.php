4f2ed2a145f6e82439926d781c160cc600d12e76
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-08 23:53:16 +0530

    Initial Commit

---
+++ Widgets.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Apps\Tms\Components\System\Storages;
+
+use System\Base\Providers\ModulesServiceProvider\Modules\Components\ComponentsWidgets;
+
+class Widgets extends ComponentsWidgets
+{}
